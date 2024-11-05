3c2a67d5d08bd11984fd4d957756fc8835e9adf3
Nataliia Skybun <nataliia.skybun@studio.unibo.it>
2024-11-05 10:24:32 +0100

    Modified application in Gradle

--- 01-interfaces/build.gradle.kts
+++ 01-interfaces/build.gradle.kts
@@ -4,20 +4,5 @@ plugins {
 }
 
 application {
-    // Set default main class
-    mainClass.set("it.unibo.sampleapp.TestBankAccount")
-}
-
-// Task personalizzato per eseguire la classe TestBankAccount
-tasks.register("runBankAccount", JavaExec) {
-    group = "application"
-    mainClass.set("it.unibo.encapsulation.interfaces.TestBankAccount")
-    classpath = sourceSets.main.runtimeClasspath
-}
-
-// Task personalizzato per eseguire la classe TestSimpleBankAccount
-tasks.register("runSimpleBank", JavaExec) {
-    group = "application"
     mainClass.set("it.unibo.encapsulation.interfaces.TestSimpleBankAccount")
-    classpath = sourceSets.main.runtimeClasspath
 }
