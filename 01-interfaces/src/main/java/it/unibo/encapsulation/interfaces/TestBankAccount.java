21e40534b9b03f45a0d577fdf38eaa244cceb1c3
Nataliia Skybun <nataliia.skybun@studio.unibo.it>
2024-11-05 11:06:59 +0100

    Implement tests for classes of Bank Account interface

--- 01-interfaces/src/main/java/it/unibo/encapsulation/interfaces/TestBankAccount.java
+++ 01-interfaces/src/main/java/it/unibo/encapsulation/interfaces/TestBankAccount.java
@@ -6,45 +6,28 @@ public class TestBankAccount {
     }
 
     public static void main(final String[] args) {
-        /*
-         * 1) Creare l' AccountHolder relativo a Andrea Rossi con id 1
-         */
-        /*
-         * 2) Creare l' AccountHolder relativo a Alex Bianchi con id 2
-         */
-        /*
-         * 3) Dichiarare due variabili di tipo BankAccount ed inizializzarle,
-         * rispettivamente, con oggetti di tipo SimpleBankAccount per il conto di
-         * Rossi (ammontare iniziale = 0), e di tipo StrictBankAccount per il conto di
-         * Bianchi (ammontare iniziale = 0)
-         */
-        /*
-         * 4) Prima riflessione: perché è stato possibile fare la new di due classi
-         * diverse in variabili dello stesso tipo?
-         */
-        /*
-         * 5) Depositare €10000 in entrambi i conti
-         */
-        /*
-         * 6) Prelevare €15000$ da entrambi i conti
-         */
-        /*
-         * 7) Stampare in stdout l'ammontare corrente
-         */
-        /*
-         * 8) Qual è il risultato e perché?
-         */
-        /*
-         * 9) Depositare nuovamente €10000 in entrambi i conti
-         */
-        /*
-         * 10) Invocare il metodo computeManagementFees su entrambi i conti
-         */
-        /*
-         * 11) Stampare a video l'ammontare corrente
-         */
-        /*
-         * 12) Qual è il risultato e perché?
-         */
+
+        AccountHolder andreaRossi = new AccountHolder("Andrea", "Rossi", 1);
+        AccountHolder alexBianchi = new AccountHolder("Alex", "Bianchi", 2);
+
+        BankAccount andreaRossiAccount = new SimpleBankAccount(andreaRossi.getUserID(), 0);
+        BankAccount alexBianchiAccount = new StrictBankAccount(alexBianchi.getUserID(), 0);
+        
+
+        andreaRossiAccount.deposit(1, 10000);
+        alexBianchiAccount.deposit(2, 10000);
+        andreaRossiAccount.withdraw(1,15000);
+        alexBianchiAccount.withdraw(1,15000);
+
+        System.out.println(andreaRossiAccount.getBalance()); // -5000
+        System.out.println(alexBianchiAccount.getBalance()); // 10.000
+
+        andreaRossiAccount.deposit(1, 10000);
+        alexBianchiAccount.deposit(2, 10000);
+        alexBianchiAccount.chargeManagementFees(1);
+        alexBianchiAccount.chargeManagementFees(2);
+
+        System.out.println(andreaRossiAccount.getBalance()); // 5000
+        System.out.println(alexBianchiAccount.getBalance()); // ?
     }
 }
