6a831e225583b0bbdc5b211e57fb32dcd945aba6
Nataliia Skybun <nataliia.skybun@studio.unibo.it>
2024-11-05 20:56:52 +0100

    Add interface Shape of generic two-dimensional geometric figure

---
+++ 03-design-from-scratch/src/main/java/it/unibo/shapes/api/Shape.java
@@ -0,0 +1,11 @@
+package it.unibo.shapes.api;
+
+/**
+ * Generic two-dimensional geometric figure.
+ */
+public interface Shape {
+    double calculateArea();
+
+    double calculatePerimeter();
+    
+}
